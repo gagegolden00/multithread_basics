e66e2c543628060862398194529d25ed10d805fd
gagegolden00 <malcolm.golden0@gmail.com>
2023-08-19 21:19:18 -0500

    git init

---
+++ lib/multithread_basics.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "multithread_basics/version"
+
+module MultithreadBasics
+  class Error < StandardError; end
+
+end
---
+++ lib/multithread_basics/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MultithreadBasics
+  VERSION = "0.1.0"
+end
