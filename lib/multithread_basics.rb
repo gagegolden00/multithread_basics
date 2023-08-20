9efc724a3cb1628d32e988767675abbba5d6f04f
gagegolden00 <malcolm.golden0@gmail.com>
2023-08-19 21:53:08 -0500

    check imput started no testing yet

--- lib/multithread_basics.rb
+++ lib/multithread_basics.rb
@@ -5,4 +5,43 @@ require_relative "multithread_basics/version"
 module MultithreadBasics
   class Error < StandardError; end
 
+  class Timer  ## TIMER CLASS =================================
+    def initialize(time)
+      @time = time
+      check_input(@time)
+      run_timer(@time)
+    end
+
+    private ## PRIVATE TIMER CLASS METHODS
+
+    def check_input(input)
+      begin
+        unless input.is_a?(Float)
+          raise ArgumentError.new("Input must be an integer value / whole number")
+        end
+      rescue ArgumentError => e
+        puts e.message
+      end
+      puts "#{input} is a Float value"
+    end
+
+    def run_timer(input)
+      @time = input.to_f
+
+      unless !@time == 0
+
+      @time.to_i.times do |i|
+        @time -= 0.10  # Decrement the time by 0.10
+
+        puts @time
+        puts
+      end
+    end
+    end
+
+  end
 end
+
+
+MultithreadBasics::Timer.new(10.0)
+
